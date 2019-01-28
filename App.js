401562485c20a2e4639a3b0421f57aa3d441a77f
mcova92 <mcova1392@gmail.com>
2019-01-27 18:04:04 -0700

    adding map

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import MapView from 'react-native-maps'
 import Router from 'react-native-easy-router'
-import { StyleSheet, Text, View, Button, Image } from 'react-native'
+import { 
+  StyleSheet,
+  Text, 
+  View, 
+  Button, 
+  Image, 
+  Platform, 
+  ScrollView,
+  TouchableOpacity 
+} from 'react-native'
 import Logo from './assets/tree.jpg';
 
 const First = ({ router }) => (
@@ -35,55 +45,100 @@ const Second = ({ router, name }) => (
     <View style={styles.container}>
       <Text>Second screen</Text>
       <Text>Hello {name}!</Text>
-      <Text onPress={() => router.pop()}>Go back</Text>
+      <Button
+        title="Recycle near me"
+        style={styles.button}
+        onPress={() => router.push.Third()}
+        color="#FAE042"
+        />
     </View>
     
   </View>
 )
 
-const routes = { First, Second }
+const Third = ({ router }) => (
+  <View accessible={true} style={styles.mapcontainer}>
+    <MapView style={ styles.map }
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }}
+      />
+  
+  </View>
+
+  // <View style={{ backgroundColor: 'white', flex: 1 }}>
+  //   <View style={styles.container}>
+  //     <Text>Second screen</Text>
+
+  //     <Text onPress={() => router.pop()}>Go back</Text>
+  //   </View>
+
+  // </View>
+);
+
+const routes = { First, Second, Third }
 const App = () => <Router routes={routes} initialRoute="First" />
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,    
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    width: '100%',
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    width: "100%"
+  },
+  mapcontainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+
+  map: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
   },
 
   h1: {
-    color: '#008F68',
-    fontSize: 40,
+    color: "#008F68",
+    fontSize: 40
   },
 
   h2: {
-    color: '#FAE042',
+    color: "#FAE042",
     fontSize: 18,
-    marginTop: 8,
+    marginTop: 8
   },
   image: {
     width: 300,
     height: 300,
-    justifyContent: 'center',
+    justifyContent: "center"
   },
   topContainer: {
     flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   middleContainer: {
     flex: 3,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    justifyContent: "flex-start",
+    alignItems: "center"
   },
   bottomContainer: {
-    justifyContent: 'flex-end',
-    width: '90%',
+    justifyContent: "flex-end",
+    width: "90%",
     margin: 20,
-    padding: 10,
-  },
+    padding: 10
+  }
 });
 
 
